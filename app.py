b653df08ce6efa9e04883a2c5a4c4275e3a4cecb
amirhossinpython <151163714+amirhossinpython@users.noreply.github.com>
2025-02-27 12:11:41 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,52 +1,45 @@
-from flask import Flask,request,jsonify,render_template
-from eitaa_bot import EitaaBot
+from flask import Flask, request, jsonify, render_template  
+from eitaa_bot import EitaaBot  
 from datetime import datetime  
 from khayyam import JalaliDatetime  
 import pytz  
 
-app =Flask(__name__)
+app = Flask(__name__)  
 
-token = ""
-bot = EitaaBot(token=token)
+token = "توکن"  
+bot = EitaaBot(token=token)  
 
-chat_id = "id"
-
-utc_now = datetime.now(pytz.utc)  
-tehran_tz = pytz.timezone('Asia/Tehran') 
-tehran_now = utc_now.astimezone(tehran_tz) 
+chat_id = "چت ایدی"  
 
 
-jalali_now = JalaliDatetime(tehran_now)  
+def get_jalali_time():  
+    utc_now = datetime.now(pytz.utc)  
+    tehran_tz = pytz.timezone('Asia/Tehran')   
+    tehran_now = utc_now.astimezone(tehran_tz)   
+    jalali_now = JalaliDatetime(tehran_now)  
+    return jalali_now.strftime("%Y/%m/%d"), jalali_now.strftime("%H:%M")  
 
+@app.route("/")  
+def home():  
+    return render_template("index.html")  
 
-shamsi_date = jalali_now.strftime("%Y/%m/%d")  
-shamsi_time = jalali_now.strftime("%H:%M")  
-
-
-
-@app.route("/")
-def home():
-    return render_template("index.html")
-
-@app.route("/send_message", methods=["POST"])
-def send_message():
-  
+@app.route("/send_message", methods=["POST"])  
+def send_message():  
+    message = request.form.get("message")  
     
-    message = request.form.get("message")
-    message = f"ناشناس به امیرحسین  👤:\n{message} ⏰\nتاریخ: {shamsi_date} 📅\nساعت: {shamsi_time} ⏲️"  
+    if not message:  
+        return jsonify({"status": "error", "message": "لطفاً پیام خود را وارد کنید."})  
     
-    if not message:
-        return jsonify({"status": "error", "message": "لطفاً پیام خود را وارد کنید."})
     
-    try :
-        bot.send_message(chat_id=chat_id, message=message)  
-        
-        return jsonify({"status": "success", "message": "پیام شما با موفقیت ارسال شد!"})
-    except Exception as e:
-        return jsonify({"status": "error", "message": f"خطا در ارسال پیام: {str(e)}"})
+    shamsi_date, shamsi_time = get_jalali_time()  
+    
+    final_message = f"ناشناس به امیرحسین  👤:\n{message} ⏰\nتاریخ: {shamsi_date} 📅\nساعت: {shamsi_time} ⏲️"  
 
-if __name__ == "__main__":
-    app.run(debug=True)
-    
-                               
+    try:  
+        bot.send_message(chat_id=chat_id, message=final_message)  
+        return jsonify({"status": "success", "message": "پیام شما با موفقیت ارسال شد!"})  
+    except Exception as e:  
+        return jsonify({"status": "error", "message": f"خطا در ارسال پیام: {str(e)}"})  
 
+if __name__ == "__main__":  
+    app.run(debug=True)  
